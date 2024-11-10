4e97914efc134714d9dedfa6d5494843a51b28e1
bilaapd <nabilapuspitadewi@mail.ugm.ac.id>
2024-11-10 14:31:52 +0700

    fix : Buat Penawaran

--- Friperies_2/buatPenawaranForm.Designer.cs
+++ Friperies_2/buatPenawaranForm.Designer.cs
@@ -28,12 +28,287 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "buatPenawaranForm";
+            TextBox tbBuatpenawaranhrg;
+            panel1 = new Panel();
+            btnExit = new Button();
+            btnLogout = new Button();
+            panel3 = new Panel();
+            panel2 = new Panel();
+            btnExit2 = new Button();
+            btnLogout2 = new Button();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            lblBuatpenawaranpage = new Label();
+            lblBuatpenawaranhrg = new Label();
+            lblBuatpenawaranhrgawal = new Label();
+            lblBuatpenawaranitem = new Label();
+            tbBuatpenawaranhrgawal = new TextBox();
+            tbBuatpenawaranitem = new TextBox();
+            btnHome = new Button();
+            btnBuatpenawaran = new Button();
+            btnBuatpenawaranbatal = new Button();
+            tbBuatpenawaranhrg = new TextBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(btnExit);
+            panel1.Controls.Add(btnLogout);
+            panel1.Controls.Add(panel3);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 0);
+            panel1.TabIndex = 4;
+            // 
+            // btnExit
+            // 
+            btnExit.FlatAppearance.BorderSize = 0;
+            btnExit.FlatStyle = FlatStyle.Flat;
+            btnExit.Font = new Font("Verdana", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnExit.Image = Properties.Resources.cancel_5697779__1_;
+            btnExit.Location = new Point(746, 6);
+            btnExit.Margin = new Padding(2);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(44, 44);
+            btnExit.TabIndex = 8;
+            btnExit.Text = " ";
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            btnLogout.FlatAppearance.BorderSize = 0;
+            btnLogout.FlatStyle = FlatStyle.Flat;
+            btnLogout.Font = new Font("Verdana", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnLogout.Image = Properties.Resources.logout_4032999__1___1_;
+            btnLogout.Location = new Point(705, 6);
+            btnLogout.Margin = new Padding(2);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(44, 44);
+            btnLogout.TabIndex = 7;
+            btnLogout.Text = " ";
+            btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.BackgroundImage = Properties.Resources.Screenshot_2024_10_20_at_17_40_01_removebg_preview__1_;
+            panel3.Location = new Point(8, 8);
+            panel3.Margin = new Padding(2);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(166, 42);
+            panel3.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.BorderStyle = BorderStyle.Fixed3D;
+            panel2.Controls.Add(btnExit2);
+            panel2.Controls.Add(btnLogout2);
+            panel2.Controls.Add(panel4);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Margin = new Padding(2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(800, 60);
+            panel2.TabIndex = 6;
+            // 
+            // btnExit2
+            // 
+            btnExit2.FlatAppearance.BorderSize = 0;
+            btnExit2.FlatStyle = FlatStyle.Flat;
+            btnExit2.Font = new Font("Verdana", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnExit2.Image = Properties.Resources.cancel_5697779__1_;
+            btnExit2.Location = new Point(746, 6);
+            btnExit2.Margin = new Padding(2);
+            btnExit2.Name = "btnExit2";
+            btnExit2.Size = new Size(44, 44);
+            btnExit2.TabIndex = 8;
+            btnExit2.Text = " ";
+            btnExit2.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout2
+            // 
+            btnLogout2.FlatAppearance.BorderSize = 0;
+            btnLogout2.FlatStyle = FlatStyle.Flat;
+            btnLogout2.Font = new Font("Verdana", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnLogout2.Image = Properties.Resources.logout_4032999__1___1_;
+            btnLogout2.Location = new Point(705, 6);
+            btnLogout2.Margin = new Padding(2);
+            btnLogout2.Name = "btnLogout2";
+            btnLogout2.Size = new Size(44, 44);
+            btnLogout2.TabIndex = 7;
+            btnLogout2.Text = " ";
+            btnLogout2.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            panel4.BackgroundImage = Properties.Resources.Screenshot_2024_10_20_at_17_40_01_removebg_preview__1_;
+            panel4.Location = new Point(8, 8);
+            panel4.Margin = new Padding(2);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(166, 42);
+            panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.FromArgb(192, 192, 255);
+            panel5.Controls.Add(btnBuatpenawaranbatal);
+            panel5.Controls.Add(lblBuatpenawaranpage);
+            panel5.Controls.Add(btnBuatpenawaran);
+            panel5.Controls.Add(lblBuatpenawaranhrg);
+            panel5.Controls.Add(lblBuatpenawaranhrgawal);
+            panel5.Controls.Add(lblBuatpenawaranitem);
+            panel5.Controls.Add(tbBuatpenawaranhrg);
+            panel5.Controls.Add(tbBuatpenawaranhrgawal);
+            panel5.Controls.Add(tbBuatpenawaranitem);
+            panel5.Controls.Add(btnHome);
+            panel5.Dock = DockStyle.Bottom;
+            panel5.Location = new Point(0, 60);
+            panel5.Margin = new Padding(2);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(800, 560);
+            panel5.TabIndex = 7;
+            // 
+            // lblBuatpenawaranpage
+            // 
+            lblBuatpenawaranpage.AutoSize = true;
+            lblBuatpenawaranpage.Font = new Font("Segoe UI Semibold", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblBuatpenawaranpage.Location = new Point(64, 59);
+            lblBuatpenawaranpage.Name = "lblBuatpenawaranpage";
+            lblBuatpenawaranpage.Size = new Size(185, 31);
+            lblBuatpenawaranpage.TabIndex = 15;
+            lblBuatpenawaranpage.Text = "Buat Penawaran";
+            // 
+            // lblBuatpenawaranhrg
+            // 
+            lblBuatpenawaranhrg.AutoSize = true;
+            lblBuatpenawaranhrg.Location = new Point(64, 252);
+            lblBuatpenawaranhrg.Name = "lblBuatpenawaranhrg";
+            lblBuatpenawaranhrg.Size = new Size(132, 20);
+            lblBuatpenawaranhrg.TabIndex = 12;
+            lblBuatpenawaranhrg.Text = "Harga Penawaran :";
+            // 
+            // lblBuatpenawaranhrgawal
+            // 
+            lblBuatpenawaranhrgawal.AutoSize = true;
+            lblBuatpenawaranhrgawal.Location = new Point(64, 198);
+            lblBuatpenawaranhrgawal.Name = "lblBuatpenawaranhrgawal";
+            lblBuatpenawaranhrgawal.Size = new Size(169, 20);
+            lblBuatpenawaranhrgawal.TabIndex = 11;
+            lblBuatpenawaranhrgawal.Text = "Harga Penawaran Awal :";
+            lblBuatpenawaranhrgawal.Click += lblUnggahdesitem_Click;
+            // 
+            // lblBuatpenawaranitem
+            // 
+            lblBuatpenawaranitem.AutoSize = true;
+            lblBuatpenawaranitem.Location = new Point(64, 143);
+            lblBuatpenawaranitem.Name = "lblBuatpenawaranitem";
+            lblBuatpenawaranitem.Size = new Size(106, 20);
+            lblBuatpenawaranitem.TabIndex = 10;
+            lblBuatpenawaranitem.Text = "Nama Produk :";
+            lblBuatpenawaranitem.Click += lblBuatpenawaranitem_Click;
+            // 
+            // tbBuatpenawaranhrg
+            // 
+            tbBuatpenawaranhrg.Location = new Point(281, 252);
+            tbBuatpenawaranhrg.Name = "tbBuatpenawaranhrg";
+            tbBuatpenawaranhrg.Size = new Size(221, 27);
+            tbBuatpenawaranhrg.TabIndex = 9;
+            // 
+            // tbBuatpenawaranhrgawal
+            // 
+            tbBuatpenawaranhrgawal.Location = new Point(281, 195);
+            tbBuatpenawaranhrgawal.Name = "tbBuatpenawaranhrgawal";
+            tbBuatpenawaranhrgawal.Size = new Size(221, 27);
+            tbBuatpenawaranhrgawal.TabIndex = 8;
+            tbBuatpenawaranhrgawal.TextChanged += tbKtgitem_TextChanged;
+            // 
+            // tbBuatpenawaranitem
+            // 
+            tbBuatpenawaranitem.Location = new Point(281, 143);
+            tbBuatpenawaranitem.Name = "tbBuatpenawaranitem";
+            tbBuatpenawaranitem.Size = new Size(221, 27);
+            tbBuatpenawaranitem.TabIndex = 6;
+            // 
+            // btnHome
+            // 
+            btnHome.BackColor = Color.FromArgb(192, 192, 255);
+            btnHome.FlatAppearance.BorderSize = 3;
+            btnHome.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnHome.Image = Properties.Resources.house_10723029__1_;
+            btnHome.Location = new Point(718, 478);
+            btnHome.Margin = new Padding(2);
+            btnHome.Name = "btnHome";
+            btnHome.Size = new Size(62, 62);
+            btnHome.TabIndex = 1;
+            btnHome.TextAlign = ContentAlignment.BottomCenter;
+            btnHome.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnHome.UseVisualStyleBackColor = false;
+            // 
+            // btnBuatpenawaran
+            // 
+            btnBuatpenawaran.Location = new Point(353, 384);
+            btnBuatpenawaran.Name = "btnBuatpenawaran";
+            btnBuatpenawaran.Size = new Size(149, 36);
+            btnBuatpenawaran.TabIndex = 14;
+            btnBuatpenawaran.Text = "Buat Penawaran";
+            btnBuatpenawaran.UseVisualStyleBackColor = true;
+            // 
+            // btnBuatpenawaranbatal
+            // 
+            btnBuatpenawaranbatal.BackColor = Color.FromArgb(255, 128, 128);
+            btnBuatpenawaranbatal.Location = new Point(167, 384);
+            btnBuatpenawaranbatal.Name = "btnBuatpenawaranbatal";
+            btnBuatpenawaranbatal.Size = new Size(149, 36);
+            btnBuatpenawaranbatal.TabIndex = 16;
+            btnBuatpenawaranbatal.Text = "Batalkan Penawaran";
+            btnBuatpenawaranbatal.UseVisualStyleBackColor = false;
+            // 
+            // buatPenawaranForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 620);
+            Controls.Add(panel5);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "buatPenawaranForm";
+            Text = "buatPenawaranForm";
+            Load += buatPenawaranForm_Load;
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button btnExit;
+        private Button btnLogout;
+        private Panel panel3;
+        private Panel panel2;
+        private Button btnExit2;
+        private Button btnLogout2;
+        private Panel panel4;
+        private Panel panel5;
+        private Label lblBuatpenawaranpage;
+        private Label lblBuatpenawaranhrg;
+        private Label lblBuatpenawaranhrgawal;
+        private Label lblBuatpenawaranitem;
+        private TextBox tbBuatpenawaranhrg;
+        private TextBox tbBuatpenawaranhrgawal;
+        private TextBox tbBuatpenawaranitem;
+        private Button btnHome;
+        private Button btnBuatpenawaran;
+        private Button btnBuatpenawaranbatal;
     }
 }
